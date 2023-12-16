30039f8c8f2ac8f9d53082113478b234c47091bb
GiftSibiya <sibiya.bobo@gmail.com>
2023-12-16 10:01:32 +0200

    Middle stuff

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Platform } from "react-native";
+import { StyleSheet, Text, View, Image, Button, Alert } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>This updating v3,</Text>
+      <Image source={require("./assets/favicon.png")} />
+      <Button
+        title="Tap Me"
+        color={"black"}
+        onPress={() => console.log("You out here pressing buttons, bruh")}
+      />
     </View>
   );
 }
@@ -13,8 +19,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
